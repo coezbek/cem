1e1b712ca5c40aa9b8e2bb8e5e185f2e4f697916
Christopher Oezbek <c.oezbek@gmail.com>
2021-12-07 13:30:57 +0100

    Remove whitespace

--- lib/cem/ccommon.rb
+++ lib/cem/ccommon.rb
@@ -20,17 +20,17 @@ end
 # expected may be kept empty, in which case the msg is adjusted
 #
 def peql(actual, expected=:unknown_peql_token, msg=nil)
-  
+
   if expected == :unknown_peql_token
     puts "#{msg}: Didn't know what to expect and got '#{actual}'"
-  else  
+  else
     if actual == expected
       puts "#{msg}: Expected '#{expected}' and got it!"
     else
       puts "#{msg}: Expected '#{expected}', but got '#{actual}!"
     end
   end
-  
+
 end
 
 class File
@@ -53,19 +53,19 @@ class Module
   # Why? Enumerable has too many methods to include at once.
   def append_from( module_to_append_from, *methods_to_keep )
     methods_to_keep.map! { |m| m.to_sym }
-    
+
     methods_to_remove = module_to_append_from.instance_methods(false) - methods_to_keep
-        
+
     new_mod = Module.new
     new_mod.module_eval do
       include module_to_append_from
       methods_to_remove.each { |meth| undef_method meth }
     end
-    
+
     # Sanity check:
     check = methods_to_keep - new_mod.instance_methods(true)
     raise "The following methods are not in the given module: #{check.inspect}" if !check.empty?
-    
+
     include new_mod
   end
 end
@@ -73,9 +73,9 @@ end
 
 class Array
 	def with_progress(&block)
-  
-    crequire 'progress_bar' 
-  
+
+    crequire 'progress_bar'
+
 		bar = ProgressBar.new(count)
 		if block
 			each{|obj| yield(obj).tap{bar.increment!}}
@@ -109,8 +109,8 @@ end
 #    -m    translate from a WSL path to a Windows path, with '/' instead of '\'
 #
 def wslpath(path, mode = "")
-  return path if !wsl? 
-  
+  return path if !wsl?
+
   return `wslpath #{mode} '#{path}'`.strip
 end
 
@@ -119,7 +119,7 @@ def isHTML5Element(element)
   # https://html.spec.whatwg.org/#elements-3
 
   @valid_elements ||= %w(a abbr address area article aside audio b base bdi bdo blockquote body br button canvas caption cite code col colgroup data datalist dd del details dfn dialog div dl dt em embed fieldset figcaption figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd label legend li link main map mark math menu meta meter nav noscript object ol optgroup option output p param picture pre progress q rp rt ruby s samp script section select slot small source span strong style sub summary sup svg table tbody td template textarea tfoot th thead time title tr track u ul var video wbr)
-    
+
   return @valid_elements.include?(element.to_s.downcase)
 
 end
